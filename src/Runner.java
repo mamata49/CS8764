ce9aa4d7e5335a6b6a6ea8cc5e8e7b8667e2e267
Mamata Prabhu <mamata49@ccs.neu.edu>
2015-06-07 21:56:48 -0400

    Query creation using sparql

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,3 @@
-package ccs.neu.edu.kids.mproj;
 
 import java.util.HashMap;
 import java.util.List;
@@ -7,80 +6,108 @@ import java.util.Random;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
-import ccs.neu.edu.kids.mproj.Property;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
-public class Runner {
 
+public class Runner{
+	
+
+	static Model model = ModelFactory.createDefaultModel();
+	static String has_property = "HAS_PROPERTY";
+	
 	public static void main (String args[]){
-
 		int i = 0;
 		CSVParser csvParser = new CSVParser();
-		HashMap<String,List<Property>> classPropertyMap = csvParser.parseCSV("/Users/prasanna/Desktop/rdf_files_parsed_A.csv");
-		Map<String,List<Property>> tempMap;
+		HashMap<String,List<Property1>> classPropertyMap = csvParser.parseCSV("C:/Back to college/Summer 2015/Master s Project/rdf_files_parsed_A.csv");
+		Map<String,List<Property1>> tempMap;
 		//A temp map to mark class in map as visited 
-		
-		for (Map.Entry<String, List<Property>> entry : classPropertyMap.entrySet()) {
-			tempMap = new HashMap<String, List<Property>>(classPropertyMap);
+		for (Map.Entry<String, List<Property1>> entry : classPropertyMap.entrySet()) {
+			tempMap = new HashMap<String, List<Property1>>(classPropertyMap);
 			createTriples(entry.getKey(),tempMap,i,classPropertyMap);
 			i++;
 		}
+		//print_details(model);
+		String queryString = 
+				"SELECT ?name_property WHERE {?name_property <isCapacityOf> \"isCapacityOf0\"}" ;
+		Query query = QueryFactory.create(queryString) ;
+		
+		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		try {
+            ResultSet rs = qexec.execSelect() ;
+            while(rs.hasNext()) {
+            	System.out.println("Name: ") ;
+                QuerySolution rb = rs.nextSolution() ;
+                //System.out.println(rb + "\n");
+                
+                RDFNode x = rb.get("name_property") ;
+                System.out.println("   "+x) ;
+            }
+        } catch(Exception c)  {
+        	System.out.println(c);
+        }
+		finally{
+			qexec.close() ;
+        }
 	}
+        
 
 	private static void createTriples(String className,
-			Map<String, List<Property>> tempMap, int i,
-			HashMap<String, List<Property>> classPropertyMap) {
+			Map<String, List<Property1>> tempMap, int i,
+			HashMap<String, List<Property1>> classPropertyMap) {
 		if(tempMap.isEmpty())
-			return;
-		for(Property p : classPropertyMap.get(className))
+		{
+			/*System.out.println("return null");
+			Model model1 = ModelFactory.createDefaultModel();
+			Resource nullR = model1.createResource("null");
+			return nullR;*/
+			return ;
+		}
+		for(Property1 p : classPropertyMap.get(className))
 		{
 			if(p.isObjectProperty())
 			{
-				createTriples(p.getType(), tempMap, i, classPropertyMap);;
-				System.out.println(className + "#" + i + " " + p.getName() + " " + p.getName() + "#" + i);
+				//Resource r1 = createTriples(p.getType(), tempMap, i, classPropertyMap);
+				Property name_property = model.getProperty(p.getName());
+				//System.out.println(className + "#" + i + " " + p.getName() + " " + p.getName() + "#" + i);
 				tempMap.remove(p.getType());
+				Property hp = model.getProperty(has_property);
+				Resource r = model.createResource(className+"#"+i)
+						.addProperty(name_property, p.getName() + i);
+					//	.addProperty(hp, r1);
+				
 			} else {
-				System.out.println(className + "#" + i + " " + p.getName() + " " + getObjectValue(p));
+				//System.out.println(className + "#" + i + " " + p.getName() + " " + getObjectValue(p));
+				Property name_property = model.getProperty(p.getName());
+				Resource r1 = model.createResource(className +"#"+i)
+						.addProperty(name_property, p.getName() + i);
+				//return r1;
 			}
 		}
+		//Model model1 = ModelFactory.createDefaultModel();
+		//Resource nullR = model1.createResource("null");
+		//System.out.println("returns after for");
+		return;
 	}
 
-	private static String getObjectValue(Property p) {
+	private static String getObjectValue(Property1 p) {
 		if(p.getType().equals("String")){
 		 return RandomStringUtils.randomAlphabetic(p.getMaxValue());	
 		}
 		Random r = new Random();
 		return Integer.toString(r.nextInt(p.getMaxValue()));
 	}
-}
-	/*
-	private static void generateRandomData(HashMap<String,List<Property>> classPropertyMap) {
-		
-		Model model = ModelFactory.createDefaultModel();
-		//Model model1 = ModelFactory.createDefaultModel();
-
-		for (Map.Entry<String, List<Property>> entry : classPropertyMap.entrySet()) {
-			Resource src = createResource(entry.getKey(),entry.getValue(),model,classPropertyMap);
-		}
-
-		RDFDataMgr.write(System.out, model, Lang.NTRIPLES) ;
-		//print_details(model);
-
-	}
-
-	private static Resource createResource(String key, List<Property> value, Model model, HashMap<String, List<Property>> classPropertyMap) {
-		Resource tempResource = model.createResource(key);
-		com.hp.hpl.jena.rdf.model.Property tmpProperty = model.getProperty(key);
-		
-		for(ccs.neu.edu.kids.mproj.Property property : value){
-			if(classPropertyMap.containsKey(property.getName())){
-				createResource(property.getName(), classPropertyMap.get(property.getName()), model, classPropertyMap);
-			} else{
-				tempResource.addProperty(tmpProperty,"1");
-			}
-		}
-		return tempResource;		
-	}
-
 	public static void print_details(Model model){
 		// list the statements in the graph
 		StmtIterator iter = model.listStatements();
@@ -91,7 +118,7 @@ public class Runner {
 			com.hp.hpl.jena.rdf.model.Resource  subject   = stmt.getSubject();   // get the subject
 			com.hp.hpl.jena.rdf.model.Property  predicate = stmt.getPredicate(); // get the predicate
 			RDFNode   object    = stmt.getObject();    // get the object
-
+			
 			System.out.print(subject.toString());
 			System.out.print(" " + predicate.toString() + " ");
 			if (object instanceof Resource) {
@@ -100,9 +127,8 @@ public class Runner {
 				// object is a literal
 				System.out.print(" \"" + object.toString() + "\"");
 			}
-			System.out.println(" .");
+			System.out.println(".");
 		}
 	}
 }
-*/
 
